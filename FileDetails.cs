26fc75eb6b4d98677028f0f4392afc65ed2b632c
madhurik28 <87888567+madhurik28@users.noreply.github.com>
2021-07-24 11:15:28 +0530

    Add files via upload

---
+++ FileDetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace FileData
+{
+	class FileDetails
+	{
+		private static Random random = new Random();
+		public static string Version(string random="")
+		{
+			try
+			{
+				Random ran = new Random();
+				random = ran.Next().ToString();
+				return random;
+			}catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		public static string Size(string random = "")
+		{
+			try
+			{
+				var thisApp = Assembly.GetExecutingAssembly();
+				AssemblyName name = new AssemblyName(thisApp.FullName);
+				string VersionNumber = "v. " + name.Version;
+				return VersionNumber;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Please enter parameter values.");
+				Console.Read();
+			}
+			else
+			{
+					if (args[1] == "-v" )
+					{
+						if (args[2] == "FileDetails.Version")
+						{
+							args[2] = "FileDetails.Version('1234')";
+							Console.WriteLine(args[2]);
+						}
+					}
+
+					if (args[1] == "--v" || args[1] == "/v")
+					{
+						if (args[2] == "FileDetails.Version")
+						{
+							args[2] = "FileDetails.Version('1234')";
+							Console.WriteLine(args[2]);
+						}
+					}else if (args[1] == "-s" || args[1] == "--s" || args[1] == "/s")
+					{
+						if (args[2] == "FileDetails.Size")
+						{
+							args[2] = "FileDetails.Size('1234')";
+							Console.WriteLine(args[2]);
+						}
+
+					}
+
+				Console.Read();
+			}
+		}
+	}
+}
+	
